616c59643dd83d246a6351a245704fc95a39017e
PEPLODOCUS <157981509+PEPLODOCUS@users.noreply.github.com>
2024-11-24 20:44:13 +0300

    Update script.sql
    Signed-off-by: PEPLODOCUS <157981509+PEPLODOCUS@users.noreply.github.com>

--- 10_stored_routines_as_functions/script.sql
+++ 10_stored_routines_as_functions/script.sql
@@ -1,1 +1,12 @@
 -- Write your SQL code here
+
+DELIMITER $$
+
+CREATE PROCEDURE GetCustomerDetails(IN id INT)
+BEGIN
+    SELECT id , name , address
+    FROM customers
+    WHERE id = id;
+END $$
+
+DELIMITER ;
